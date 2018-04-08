fb2d3b6bec48d6a073e328458fb089fd2956fb60
LittleElijah <cuiyeqing960904@gmail.com>
2018-04-08 16:41:33 +0300

    task due to run

--- app/Scheduler/Event.php
+++ app/Scheduler/Event.php
@@ -2,9 +2,17 @@
 
 namespace App\Scheduler;
 
+use Carbon\Carbon;
+use Cron\CronExpression;
+
 abstract class Event
 {
   public $expression = '* * * * *';
 
   abstract public function handle();
+
+  public function isDueToRun(Carbon $date)
+  {
+    return CronExpression::factory($this->expression)->isDue($date);
+  }
 }
--- tests/Unit/EventTest.php
+++ tests/Unit/EventTest.php
@@ -1,15 +1,29 @@
 <?php
 
+use Carbon\Carbon;
 use App\Scheduler\Event;
 use PHPUnit\Framework\TestCase;
 
 class EventTest extends TestCase
 {
+  // public function setUp()
+  // {
+  //
+  // }
+
   /** @test */
   public function event_has_default_cron_expression()
   {
     $event = $this->getMockForAbstractClass(Event::class);
 
     $this->assertEquals($event->expression, '* * * * *');
+  }
+
+  /** @test */
+  public function event_should_be_run()
+  {
+    $event = $this->getMockForAbstractClass(Event::class);
+
+    $this->assertTrue($event->isDueToRun(Carbon::now()));
   }
 }
