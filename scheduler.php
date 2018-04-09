826e2739a800689e71f01059a78c690c83f70c66
LittleElijah <cuiyeqing960904@gmail.com>
2018-04-09 13:18:55 +0300

    Running due tasks

--- scheduler.php
+++ scheduler.php
@@ -1,11 +1,13 @@
 <?php
 
+use Carbon\Carbon;
 use App\Events\SomeEvent;
 use App\Scheduler\Kernel;
 
 require_once 'vendor/autoload.php';
 
 $kernel = new Kernel;
+$kernel->setDate(Carbon::now()->tz('Europe/London'));
 $kernel->add(new SomeEvent())->everyMinute();
 
 $kernel->run();
