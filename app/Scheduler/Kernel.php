16ac630b433a9146ed4ebeb13a64c0fc0b0fcecc
LittleElijah <cuiyeqing960904@gmail.com>
2018-04-09 12:54:29 +0300

    set get default date

--- app/Scheduler/Kernel.php
+++ app/Scheduler/Kernel.php
@@ -2,11 +2,14 @@
 
 namespace App\Scheduler;
 
+use Carbon\Carbon;
 use App\Scheduler\Event;
 
 class Kernel
 {
   protected $events = [];
+
+  protected $date;
 
   public function getEvents()
   {
@@ -16,5 +19,19 @@ class Kernel
   public function add(Event $event)
   {
     $this->events[] = $event;
+  }
+
+  public function setDate(Carbon $date)
+  {
+    $this->date = $date;
+  }
+
+  public function getDate()
+  {
+    if (!$this->date) {
+      return Carbon::now();
+    }
+
+    return $this->date;
   }
 }
--- tests/Unit/KernelTest.php
+++ tests/Unit/KernelTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use App\Scheduler\Event;
 use App\Scheduler\Kernel;
 use PHPUnit\Framework\TestCase;
@@ -32,6 +33,23 @@ class KernelTest extends TestCase
 
     $kernel = new Kernel;
     $kernel->add('non event');
+  }
 
+  /** @test */
+  public function can_set_date()
+  {
+    $kernel = new Kernel;
+
+    $kernel->setDate(Carbon::now());
+
+    $this->assertInstanceOf(Carbon::class, $kernel->getDate());
+  }
+
+  /** @test */
+  public function has_default_date()
+  {
+    $kernel = new Kernel;
+
+    $this->assertInstanceOf(Carbon::class, $kernel->getDate());
   }
 }
